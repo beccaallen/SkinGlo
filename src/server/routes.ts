b477af4062c23e71863c4ae755db6ec592820c23
beccaallen <becca@becca.work>
2021-12-03 16:59:23 -0600

    searchbar progress

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import fetch from "node-fetch";
 import DB from './db';
 
 const router = express.Router();
@@ -23,5 +24,16 @@ router.get('/skinglo/photo/:api_id', async (req, res) => {
         res.sendStatus(500);
     }    
 });
+
+router.get("/products", async (req, res) => {
+    try {
+        let skinglo = await fetch("https://skincare-api.herokuapp.com/products");
+        const data = await skinglo.json();
+        res.json(data);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }  
+})
 
 export default router;
